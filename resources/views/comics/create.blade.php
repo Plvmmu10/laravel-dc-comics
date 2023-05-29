5d72162bd11958a7f8554c3dcbce8ff336c23777
peppeluc22 <gluca0260@gmail.com>
2023-05-29 18:09:51 +0200

    fixed some errors and add sale_date

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -9,43 +9,50 @@
                 {{-- Title --}}
                 <div class="mb-3">
                     <label for="title" class="form-label">Titolo</label>
-                    <input type="text" class="form-control" id="title">
+                    <input type="text" class="form-control" id="title" name="title">
                     <div id="title" class="form-text">Inserisci il nome del tuo fumetto</div>
                 </div>
 
                 {{-- Description --}}
                 <div class="mb-3">
                     <label for="description" class="form-label">Descrizione</label>
-                    <input type="textarea" class="form-control" id="description">
+                    <input type="textarea" class="form-control" id="description" name="description">
                     <div id="description" class="form-text">Inserisci la descrizione</div>
                 </div>
 
                 {{-- Image --}}
                 <div class="mb-3">
-                    <label for="image" class="form-label">Immagine</label>
-                    <input type="text" class="form-control" id="image">
-                    <div id="image" class="form-text">Inserisci l'url dell'immagine</div>
+                    <label for="thumb" class="form-label">Immagine</label>
+                    <input type="text" class="form-control" id="thumb" name="thumb">
+                    <div id="thumb" class="form-text">Inserisci l'url dell'immagine</div>
                 </div>
 
                 {{-- Price --}}
                 <div class="mb-3">
                     <label for="price" class="form-label">Prezzo</label>
-                    <input type="text" class="form-control" id="price">
+                    <input type="text" class="form-control" id="price" name="price">
                     <div id="price" class="form-text">Inserisci qui il prezzo del fumetto</div>
                 </div>
 
                 {{-- Series --}}
                 <div class="mb-3">
                     <label for="series" class="form-label">Serie</label>
-                    <input type="text" class="form-control" id="series">
+                    <input type="text" class="form-control" id="series" name="series">
                     <div id="series" class="form-text">Inserisci la serie del fumetto</div>
                 </div>
 
                 {{-- Type --}}
                 <div class="mb-3">
                     <label for="type" class="form-label">Tipo</label>
-                    <input type="text" class="form-control" id="type">
+                    <input type="text" class="form-control" id="type" name="type">
                     <div id="type" class="form-text">Inserisci il tipo di fumetto</div>
+                </div>
+
+                {{-- Sell Date --}}
+                <div class="mb-3">
+                    <label for="sale_date" class="form-label">Data di Vendita</label>
+                    <input type="text" class="form-control" id="type" name="sale_date">
+                    <div id="type" class="form-text">Inserisci la data di vendita YYYY-MM-DD</div>
                 </div>
 
                 <button type="submit" class="btn btn-primary">Crea</button>
