dd221cf394ea680363cfe6b7e85da42313809380
peppeluc22 <gluca0260@gmail.com>
2023-05-29 14:10:34 +0200

    init

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,17 @@
+<div class="myContainer text-black">
+    <!-- Logo -->
+    <div class="logo-container">
+        <img src="img/dc-logo.png" alt="logoDCsmall">
+    </div>
+
+    <!-- Navbar -->
+    <div class="navbar">
+
+        <ul class="mainList">
+            <li class="text-uppercase fw-bold">
+                <a href="#" class="text-decoration-none">HOME</a>
+            </li>
+        </ul>
+
+    </div>
+</div>
