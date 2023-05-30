c15c9b59821bee6ccff19b14223574c72e45f026
peppeluc22 <gluca0260@gmail.com>
2023-05-30 15:13:07 +0200

    created main

---
+++ resources/views/partials/main.blade.php
@@ -0,0 +1,28 @@
+<div class="mainContainer text-white flex-wrap">
+    <span class="text-uppercase fw-bold">current series</span>
+    <div class="btn-container d-flex flex-column justify-content-center align-items-center w-100">
+
+        <div class="row my-5">
+            @foreach ($comics as $comic)
+                @include('partials.card')
+            @endforeach
+        </div>
+        <div>
+            <button class="btn btn-primary text-uppercase rounded-0 fw-bold">load more</button>
+        </div>
+    </div>
+</div>
+
+<style lang="scss" scoped>
+    .mainContainer {
+        padding: 2rem 0;
+        position: relative;
+    }
+
+    span {
+        position: absolute;
+        top: -20px;
+        padding: .7rem 1.5rem;
+        background-color: #0282f9;
+    }
+</style>
