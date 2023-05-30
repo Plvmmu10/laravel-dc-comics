8102c31e27af9b1191e96816f133e8f9bda418fa
peppeluc22 <gluca0260@gmail.com>
2023-05-30 15:11:57 +0200

    changed structure of index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -1,34 +1,28 @@
 @extends('layouts.app')
 
+@section('title')
+    DC Comics
+@endsection
+
 @section('content')
-    <div class="container my-5">
-        <div class="d-flex justify-content-center">
-            <button class="btn btn-success"><a href="{{ route('comics.create') }}" class="text-decoration-none text-white">Crea
-                    il tuo
-                    fumetto!</a></button>
+    @include('partials.jumbotron')
+
+    @if (session()->has('message'))
+        <div class="bg-success p-3 m-2 text-white text-center text-uppercase">
+            {{ session()->get('message') }}
         </div>
-        <div class="row mt-5">
-            @foreach ($comics as $comic)
-                <div class="col-4 p-2 ">
-                    <div class="card border-0">
-                        <div class="card-header border-0">
-                            <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}" class="img-fluid w-100 h-100">
-                        </div>
-
-                        <div class="card-body position-relative">
-                            <h4>{{ $comic['title'] }}</h4>
-                            <div class="position-absolute bottom-0">
-                                <button class="btn btn-primary"><a href="{{ route('comics.show', $comic->id) }}"
-                                        class="text-decoration-none text-white">Info</a></button>
-                                <button class="btn btn-warning"><a href="{{ route('comics.edit', $comic->id) }}"
-                                        class="text-decoration-none text-white">Modifica</a></button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div>
+    @endif
 
 
+    <div class="d-flex justify-content-center p-5">
+        <button class="btn btn-success"><a href="{{ route('comics.create') }}" class="text-decoration-none text-white">Crea
+                il tuo
+                fumetto!</a></button>
     </div>
+
+    <div class="container d-flex justify-content-center">
+        @include('partials.main')
+    </div>
+
+    @include('partials.mainNav')
 @endsection
