0b7511d6cf83273a65b7398b2261c902c80bdeb9
peppeluc22 <gluca0260@gmail.com>
2023-05-30 15:13:15 +0200

    created main nav

---
+++ resources/views/partials/mainNav.blade.php
@@ -0,0 +1,58 @@
+<div class="mainNavContainer d-flex">
+    <div class="container">
+        <ul>
+            <li class="d-flex align-items-center px-3">
+                <div class="icons-box">
+                    <img src="/img/buy-comics-digital-comics.png">
+                </div>
+
+                <a href="#">
+                    <p class="icons-text text-uppercase text-white">
+                        icon
+                    </p>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
+
+
+<style>
+    .icons-box {
+        width: 70;
+        height: 70px;
+        margin: 0 .5rem;
+    }
+
+    .icons-box img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+        object-position: center;
+    }
+
+    .icons-text {
+        font-size: 16px;
+        margin: 0;
+    }
+
+    .mainNavContainer {
+        align-items: center;
+        background-color: #0282f9;
+    }
+
+    .mainNavContainer ul {
+        padding: 1rem;
+        display: flex;
+        flex-wrap: wrap;
+    }
+
+    .mainNavContainer li {
+        cursor: pointer;
+        padding: 10px;
+    }
+
+    .mainNavContainer a {
+        color: white;
+    }
+</style>
